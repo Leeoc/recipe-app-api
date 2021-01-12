4c633e742f6805794c575e5e8a67800e414c324d
Lee O'Connell <leeoconnell@Lees-MacBook-Pro.local>
2021-01-12 15:45:45 +0000

    added endpoint for creating and auth users

--- app/user/views.py
+++ app/user/views.py
@@ -1,5 +1,7 @@
 # from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from . import serializers
 
@@ -7,3 +9,9 @@ from . import serializers
 class CreateUserView(generics.CreateAPIView):
     """Create new user in system"""
     serializer_class = serializers.UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user"""
+    serializer_class = serializers.AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
